f58756f74e1a818c9e98175fea20a28af26b7a1d
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-16 09:32:26 +0200

    random szin generalas es kiiras

--- app/src/main/java/hu/petrik/android/szinek/MainActivity.java
+++ app/src/main/java/hu/petrik/android/szinek/MainActivity.java
@@ -2,14 +2,19 @@ package hu.petrik.android.szinek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout layout;
     private TextView textSzin;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        layout.setOnClickListener(view -> {
+            int red = rnd.nextInt(256);
+            int green = rnd.nextInt(256);
+            int blue = rnd.nextInt(256);
+            layout.setBackgroundColor(Color.rgb(red, green, blue));
+            String szinSzoveges = String.format("(%d, %d, %d)", red, green, blue);
+            textSzin.setText(szinSzoveges);
+        });
     }
 
     private void init(){
         layout = findViewById(R.id.layout);
         textSzin = findViewById(R.id.textSzin);
+        rnd = new Random();
     }
 
 }
